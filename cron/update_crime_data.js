8ddcc7f7be15bac92cc01a3a657bdfa0e93674d8
ycombinator <ycombinator@gmail.com>
2013-01-11 15:55:40 -0800

    Implementing upsert functionality.

--- cron/update_crime_data.js
+++ cron/update_crime_data.js
@@ -2,75 +2,155 @@ var nodeio = require('node.io'),
     pg = require('pg'),
     crime_data_scraper = require(__dirname + '/../lib/crime_data_scraper');
 
+
+var crimeDataUpdater = {};
+
 // TODO: Move to common file
-var connectionString = "tcp://bp:bp@localhost/louisville_crime";
+crimeDataUpdater.connectionString = "tcp://bp:bp@localhost/louisville_crime";
 
-var job = crime_data_scraper.job;
-job.output_callback = function(err, crimes) {
+crimeDataUpdater.postProcessScrapedData = function(cr) {
+    cr.ReportedDateTime = cr.DateReported + ' ' + cr.TimeReported;
+};
 
-    pg.connect(connectionString, function(err, client) {
+crimeDataUpdater.updateRecord = function(cr) {
+
+    var queryStr = "UPDATE crimes SET "
+	+ "incident_timestamp = $1, "
+	+ "reported_timestamp = $2, "
+	+ "street_address = $4, "
+	+ "city = $5, "
+	+ "zipcode = $6, "
+	+ "beat = $7, "
+	+ "category = $9, "
+	+ "division = $10, "
+	+ "sector = $11, "
+	+ "incident_beat = $12"
+	+ "WHERE case_number = $3 AND crime = $8";
+    
+    var query = this.client.query({
+	text: queryStr,
+	values: [
+	    cr.IncidentDate,
+	    cr.ReportedDateTime,
+	    cr.CaseNumber,
+	    cr.Address,
+	    cr.City,
+	    cr.ZipCode,
+	    cr.Beat,
+	    cr.Crime,
+	    cr.Category,
+	    cr.Division,
+	    cr.Sector,
+	    cr.IncidentBeat
+	]
+    });
+    
+    query.on('error', function(error) {
+	console.error('query error = ' + error);
+	process.exit(2);
+    });		     
+    
+    query.on('end', function(result) {
+	console.log("Updated crime with case number = " + cr.CaseNumber + " and crime = " + cr.Crime);
+    });
+	
+};
+
+crimeDataUpdater.insertRecord = function(cr) {
+
+    var queryStr = "INSERT INTO crimes ("
+	+ "incident_timestamp, "
+	+ "reported_timestamp, "
+	+ "case_number, "
+	+ "street_address, "
+	+ "city, "
+	+ "zipcode, "
+	+ "beat, "
+	+ "crime, "
+	+ "category, "
+	+ "division, "
+	+ "sector, "
+	+ "incident_beat"
+	+ ") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)";
+    
+    var query = this.client.query({
+	text: queryStr,
+	values: [
+	    cr.IncidentDate,
+	    cr.ReportedDateTime,
+	    cr.CaseNumber,
+	    cr.Address,
+	    cr.City,
+	    cr.ZipCode,
+	    cr.Beat,
+	    cr.Crime,
+	    cr.Category,
+	    cr.Division,
+	    cr.Sector,
+	    cr.IncidentBeat
+	]
+    });
+    
+    query.on('error', function(error) {
+	console.error('query error = ' + error);
+	process.exit(3);
+    });		     
+
+    query.on('end', function(result) {
+	console.log("Inserted crime with case number = " + cr.CaseNumber + " and crime = " + cr.Crime);
+    });
+	
+    
+};
+
+crimeDataUpdater.upsertRecord = function(cr) {
+
+    var queryStr = "SELECT 1 AS exists FROM crimes WHERE case_number = $1 AND crime = $2";
+
+    var query = this.client.query({
+	text: queryStr,
+	values: [ cr.CaseNumber, cr.Crime ]
+    });
+
+    query.on('end', function(result) {
+	if (result.rowCount === 1) {
+	    crimeDataUpdater.updateRecord(cr);
+	} else {
+	    crimeDataUpdater.insertRecord(cr);
+	}
+    });
+
+    query.on('error', function(error) {
+	console.error('query error = ' + error);
+	process.exit(4);
+    });
+
+};
+
+crimeDataUpdater.job = crime_data_scraper.job;
+crimeDataUpdater.job.output_callback = function(err, crimes) {
+
+    pg.connect(crimeDataUpdater.connectionString, function(err, client) {
 
 	if (err) {
 	    console.error(err);
 	    process.exit(1);
 	}
 
-	client.on('drain', client.end.bind(client));
-
+	crimeDataUpdater.client = client;
+	client.on('drain', function() { console.log("Client query queue drained") });
 
 	for (index in crimes) {
 	    var cr = crimes[index];
 
-	    // Post-process scraped data
-	    cr.ReportedDateTime = cr.DateReported + ' ' + cr.TimeReported;
+	    crimeDataUpdater.postProcessScrapedData(cr);
+	    crimeDataUpdater.upsertRecord(cr);
 
-	    var queryStr = "INSERT INTO crimes ("
-		+ "incident_timestamp, "
-		+ "reported_timestamp, "
-		+ "case_number, "
-		+ "street_address, "
-		+ "city, "
-		+ "zipcode, "
-		+ "beat, "
-		+ "crime, "
-		+ "category, "
-		+ "division, "
-		+ "sector, "
-		+ "incident_beat"
-		+ ") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)";
-
-	    var query = client.query({
-		text: queryStr,
-		values: [
-		    cr.IncidentDate,
-		    cr.ReportedDateTime,
-		    cr.CaseNumber,
-		    cr.Address,
-		    cr.City,
-		    cr.ZipCode,
-		    cr.Beat,
-		    cr.Crime,
-		    cr.Category,
-		    cr.Division,
-		    cr.Sector,
-		    cr.IncidentBeat
-		]
-	    });
-
-	    query.on('error', function(error) {
-		console.log('query error = ' + error);
-		process.exit(2);
-	    });		     
-
-	    query.on('end', function(result) {
-		console.log('query result = ' + result);
-	    });
-	    
 	}
 
     });
 
-}
+};
 
 // Start scraping job
-nodeio.start(job, null, job.output_callback, true);
+nodeio.start(crimeDataUpdater.job, null, crimeDataUpdater.job.output_callback, true);
