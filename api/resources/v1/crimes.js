4386f391444ca1496109f443868daf394973e3a7
ycombinator <ycombinator@gmail.com>
2013-01-13 11:48:26 -0800

    Adding /crimes resource.

---
+++ api/resources/v1/crimes.js
@@ -0,0 +1,35 @@
+var pg = require('pg'),
+    config = require('config');
+
+exports.get = function(req, res, next) {
+
+    pg.connect(config.db_connection_string, function(err, client) {
+
+	if (err) {
+	    console.error(err);
+	    res.send(500, { message: String(err) });
+	}
+
+	// Get crimes
+	var queryText = "SELECT DISTINCT(crime) FROM crimes";
+	var query = client.query({
+	    text: queryText
+	});
+
+	query.on('error', function(err) {
+	    console.error("query error = " + err);
+	    res.send(500, { message: "query error = " + String(err) });
+	});
+
+	data = { crimes: [] };
+	query.on('row', function(row) {
+	    data.crimes.push(row.crime);
+	});
+
+	query.on('end', function(result) {	    
+	    res.send(data);
+	});
+	
+    });
+    
+}
--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ server.use(restify.queryParser()); // Parse query strings
 // Server and routes
 server.get('/v1/crimes', resources.v1.crimes.get);
 server.get('/v1/neighborhoods', resources.v1.neighborhoods.get);
+server.get('/v1/incidents', resources.v1.incidents.get);
 
 server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
