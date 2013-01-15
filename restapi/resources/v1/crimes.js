d9b0057424afbf7a8583b0c66cc9d1c63ca8cfca
ycombinator <ycombinator@gmail.com>
2013-01-14 19:24:09 -0800

    Refactoring.

--- restapi/resources/v1/crimes.js
+++ restapi/resources/v1/crimes.js
@@ -1,35 +1,41 @@
 var pg = require('pg'),
-    config = require('config');
+config = require('config');
 
 exports.get = function(req, res, next) {
 
-    pg.connect(config.db_connection_string, function(err, client) {
+  pg.connect(config.db_connection_string, function(err, client) {
 
-	if (err) {
-	    console.error(err);
-	    res.send(500, { message: String(err) });
-	}
+    if (err) {
+      console.error(err);
+      res.send(500, { message: String(err) });
+    }
 
-	// Get crimes
-	var queryText = "SELECT DISTINCT(crime) FROM crimes";
-	var query = client.query({
-	    text: queryText
-	});
+    appendCrimes(client, req, res, next);
 
-	query.on('error', function(err) {
-	    console.error("query error = " + err);
-	    res.send(500, { message: "query error = " + String(err) });
-	});
+  });
 
-	data = { crimes: [] };
-	query.on('row', function(row) {
-	    data.crimes.push(row.crime);
-	});
+}
 
-	query.on('end', function(result) {	    
-	    res.send(data);
-	});
-	
-    });
-    
+var appendCrimes = function(client, req, res, next) {
+
+  // Get crimes
+  var queryText = "SELECT DISTINCT(crime) FROM crimes";
+  var query = client.query({
+    text: queryText
+  });
+
+  query.on('error', function(err) {
+    console.error("query error = " + err);
+    res.send(500, { message: "query error = " + String(err) });
+  });
+
+  data = { crimes: [] };
+  query.on('row', function(row) {
+    data.crimes.push(row.crime);
+  });
+
+  query.on('end', function(result) {      
+    res.send(data);
+  });
+  
 }
