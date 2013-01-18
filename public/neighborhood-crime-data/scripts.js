886ea1c7619494cc7e53fc31d1b20542bc084f99
Marcin Wichary <mwichary@gmail.com>
2013-01-18 11:37:41 -0800

    Initial check-in of Neighborhood Crime Data site (w/ fake data).

---
+++ public/neighborhood-crime-data/scripts.js
@@ -0,0 +1,521 @@
+var CHART_WIDTH = 50;
+
+var DATA_TRANSITION_DELAY = 150;
+
+var FILTERS = [
+  {
+    name: 'Crime types',
+    choices: [
+      { title: 'Total crime',
+        choices: [
+          { title: 'Property crime',
+            choices: [
+              { title: 'Auto theft' },
+              { title: 'Theft' },
+              { title: 'Vandalism' },
+            ]
+          },
+          { title: 'Violent crime',
+            choices: [
+              { title: 'Aggravated assault' },
+              { title: 'Assault' },
+              { title: 'Homicide' },
+              { title: 'Robbery' },
+              { title: 'Simple assault' },
+            ]
+          },
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Neighborhoods',
+    choices: [
+      { title: 'All neighborhoods',
+        choices: [
+          { title: 'Algonquin' },
+          { title: 'Audubon' },
+          { title: 'Avondale Melbourne Heights' },
+          { title: 'Bashford Manor' },
+          { title: 'Beechmont' },
+          { title: 'Belknap' },
+          { title: 'Bon Air' },
+          { title: 'Bonnycastle' },
+          { title: 'Bowman' },
+          { title: 'Brownsboro Zorn' },
+          { title: 'Butchertown' },
+          { title: 'California' },
+          { title: 'Camp Taylor' },
+          { title: 'Central Business District' },
+          { title: 'Cherokee Gardens' },
+          { title: 'Cherokee Seneca' },
+          { title: 'Cherokee Triangle' },
+          { title: 'Chickasaw' },
+          { title: 'Clifton' },
+          { title: 'Clifton Heights' },
+          { title: 'Cloverleaf' },
+          { title: 'Crescent Hill' },
+          { title: 'Deer Park' },
+          { title: 'Edgewood' },
+          { title: 'Fairgrounds' },
+          { title: 'Gardiner Lane' },
+          { title: 'Germantown' },
+          { title: 'Hallmark' },
+          { title: 'Hawthorne' },
+          { title: 'Hayfield Dundee' },
+          { title: 'Hazelwood' },
+          { title: 'Highland Park' },
+          { title: 'Highlands' },
+          { title: 'Highlands Douglas' },
+          { title: 'Hikes Point' },
+          { title: 'Irish Hill' },
+          { title: 'Iroquios Park' },
+          { title: 'Iroquois' },
+          { title: 'Jacobs' },
+          { title: 'Kenwood Hill' },
+          { title: 'Klondike' },
+          { title: 'Limerick' },
+          { title: 'Merriwether' },
+          { title: 'Old Louisville' },
+          { title: 'Paristowne Point' },
+          { title: 'Park Duvalle' },
+          { title: 'Park Hill' },
+          { title: 'Parkland' },
+          { title: 'Phoenix Hill' },
+          { title: 'Poplar Level' },
+          { title: 'Portland' },
+          { title: 'Prestonia' },
+          { title: 'Rock Creek Lexington Road' },
+          { title: 'Russell' },
+          { title: 'Saint Joseph' },
+          { title: 'Schnitzelburg' },
+          { title: 'Shawnee' },
+          { title: 'Shelby Park' },
+          { title: 'Smoketown Jackson' },
+          { title: 'South Louisville' },
+          { title: 'Southside' },
+          { title: 'Soutland Park' },
+          { title: 'Taylor Berry' },
+          { title: 'Tyler Park' },
+          { title: 'University' },
+          { title: 'Wilder Park' },
+          { title: 'Wyandotte' },
+          
+/*          
+          { title: 'Bashford Manor' },
+          { title: 'Beechmont' },
+          { title: 'Crescent Hill' },
+          { title: 'Klondike' },
+          { title: 'Old Louisville' },
+          { title: 'Saint Joseph' },
+          { title: 'Shelby Park' },
+          { title: 'Smoketown Jackson' },
+          { title: 'Southside' },
+*/
+        ]
+      }
+    ]
+  }
+];
+
+var fakeData = [];
+
+var data = [];
+var filters = [];
+
+var mapReady = false;
+
+function createFakeData() {
+  fakeData = [];
+
+  for (var i in filters[0].choices) {
+    fakeData[i] = [];
+
+    for (var j in filters[1].choices) {
+      var randomVal = filters[1].choices[j].title.charCodeAt(0) + filters[1].choices[j].title.charCodeAt(1); 
+      fakeData[i][j] = Math.floor(Math.random() * (i * randomVal)) * 3;
+    }
+  } 
+}
+
+function loadData() {
+  data = [];
+
+  data[0] = [];
+  data[1] = [];
+
+  // TODO(mwichary): Unify this
+
+  for (var i in filters[0].choices) {
+    data[0][filters[0].choices[parseInt(i)].choiceNumber] = 0;
+
+    var choice = filters[0].choices[i];
+
+    for (var ii in choice.filterList) {
+      for (var j in filters[1].choices[filters[1].selected].filterList) {
+        var jj = filters[1].choices[filters[1].selected].filterList[j];
+
+        data[0][filters[0].choices[parseInt(i)].choiceNumber] += 
+            fakeData[choice.filterList[ii]][jj];
+      }
+    }
+  }
+
+  for (var j in filters[1].choices) {
+    data[1][filters[1].choices[parseInt(j)].choiceNumber] = 0;
+
+    var choice = filters[1].choices[j];
+
+    for (var jj in choice.filterList) {
+      for (var i in filters[0].choices[filters[0].selected].filterList) {
+        var ii = filters[0].choices[filters[0].selected].filterList[i];
+
+        data[1][filters[1].choices[parseInt(j)].choiceNumber] += 
+            fakeData[ii][choice.filterList[jj]];
+      }
+    }
+  }
+}
+
+function createNav() {
+  for (var i in filters) {
+    var filter = filters[i];
+
+    var el = document.createElement('ul');
+
+    el.setAttribute('filterNumber', i);
+
+    for (var j in filter.choices) {
+      var liEl = document.createElement('li');
+      liEl.innerHTML = 
+          '<span class="name">' + filter.choices[j].title + '</span>' +
+          '<span class="value">' + '–' + '</span>' +
+          '<span class="chart"></span>';
+
+      liEl.setAttribute('level', filter.choices[j].level);
+
+      liEl.setAttribute('choiceNumber', filter.choices[j].choiceNumber);
+
+      liEl.addEventListener('click', onFilterClick, false);
+      liEl.addEventListener('mousedown', onFilterMouseDown, false);
+
+      el.appendChild(liEl);
+    }
+
+    document.querySelector('body > nav').appendChild(el);
+  }
+}
+
+function onFilterMouseDown(event) {
+  event.preventDefault();
+}
+
+function onFilterClick(event) {
+  var el = event.target;
+
+  while (el.tagName != 'LI') {
+    el = el.parentNode;
+  }
+
+  var ulEl = el.parentNode;
+
+  var filterNumber = parseInt(ulEl.getAttribute('filterNumber'));
+  var choiceNumber = parseInt(el.getAttribute('choiceNumber'));
+
+  filters[filterNumber].selected = choiceNumber;
+
+  updateData();
+}
+
+function formatNumber(number) {
+  if (number == 0) {
+    return '–';
+  } else {
+    return number.toString().replace(/\d(?=(?:\d{3})+(?!\d))/g, '$&,');
+  }
+}
+
+function updateNav() {
+  var els = document.querySelectorAll('body > nav li');
+  for (var i = 0, el; el = els[i]; i++) {
+    el.classList.remove('selected');
+    el.classList.remove('active');
+  }
+
+  for (var i in filters) {
+    var max = 0;
+
+    for (var j in filters[i].choices) {
+      var el = document.querySelector(
+          'body > nav > ul[filterNumber="' + 
+          (parseInt(i)) + '"] > li[choiceNumber="' + 
+          (filters[i].choices[j].choiceNumber) + '"] > .value');
+
+      var val = data[i][j];
+      el.innerHTML = formatNumber(val);
+
+      if (val > max) {
+        max = val;
+      }
+    }
+
+    for (var j in filters[i].choices) {
+      var el = document.querySelector(
+          'body > nav > ul[filterNumber="' + 
+          (parseInt(i)) + '"] > li[choiceNumber="' + 
+          (filters[i].choices[j].choiceNumber) + '"] > .chart');
+
+      var val = parseFloat(data[i][j]);
+      el.style.width = ((val / max) * CHART_WIDTH) + 'px';
+    }
+
+    var el = document.querySelector(
+        'body > nav > ul[filterNumber="' + 
+        (parseInt(i)) + '"] > li[choiceNumber="' + 
+        (filters[i].selected) + '"]');
+    el.classList.add('selected');
+    el.classList.add('active');
+
+    var nextEl = el.nextSibling;
+    while (nextEl && (nextEl.getAttribute('level') > el.getAttribute('level'))) {
+      nextEl.classList.add('active');
+      nextEl = nextEl.nextSibling;
+    }
+
+  }
+}
+/*
+function updateMap() {
+  var el = document.querySelector('#map');
+  el.innerHTML = '';
+
+  var max = 0;
+  for (var i = 1; i < filters[1].choices.length; i++) {
+    if (max < data[1][i]) {
+      max = data[1][i];
+    }
+  }
+
+  for (var i = 1; i < filters[1].choices.length; i++) {
+    var perc = (data[1][i] / max);
+
+    var neighborhoodEl = document.createElement('div');
+    neighborhoodEl.classList.add('neighborhood');
+    neighborhoodEl.style.opacity = perc;
+
+    el.appendChild(neighborhoodEl);
+  }
+}*/
+
+function updateData() {
+  loadData();
+  updateNav();
+  updateCaption();
+  window.setTimeout(updateMap, 0);
+}
+
+function updateCaption() {
+  document.querySelector('#caption-crime').innerHTML =
+    filters[0].choices[filters[0].selected].title;
+
+  document.querySelector('#caption-neighborhood').innerHTML =
+    filters[1].choices[filters[1].selected].title;
+}
+
+function addChoices(origData, flatData, level) {
+  for (var j in origData) {
+    var newEntry = {
+      title: origData[j].title,
+      choiceNumber: null,
+      filterList: null,
+      level: level
+    };
+
+    flatData.push(newEntry);
+
+    if (origData[j].choices) {
+      addChoices(origData[j].choices, flatData, level + 1);
+    }
+  }
+}
+
+function prepareFilters() {
+  filters = [];
+
+  for (var i in FILTERS) {
+    filters[i] = {};
+    filters[i].selected = 0;
+    filters[i].choices = [];
+  
+    addChoices(FILTERS[i].choices, filters[i].choices, 0);
+
+    var maxLevel = 0;
+    for (var j in filters[i].choices) {
+      if (filters[i].choices[j].level > maxLevel) {
+        maxLevel = filters[i].choices[j].level;
+      }
+    }
+
+    var realChoiceNumber = 0;
+    for (var j in filters[i].choices) {
+      if (filters[i].choices[j].level < maxLevel) {
+        filters[i].choices[j].choiceNumber = realChoiceNumber;
+        filters[i].choices[j].filterList = [];
+      } else {
+        filters[i].choices[j].choiceNumber = realChoiceNumber;
+        filters[i].choices[j].filterList = [realChoiceNumber];
+
+        var k = parseInt(j) - 1;
+        var level = filters[i].choices[j].level - 1;
+        while (k >= 0) {
+          if (filters[i].choices[k].level == level) {
+            filters[i].choices[k].filterList.push(parseInt(j));
+            level--;
+          }
+          k--;
+        }
+      }
+      realChoiceNumber++;
+
+    }
+  }  
+
+  /*console.log('FILTERS');
+  for (var i in filters[0].choices) {
+    console.log(filters[0].choices[i].filterList);
+  }*/
+}
+
+function prepareMap() {
+  //mapPath = d3.geo.path().projection(d3.geo.albers().scale(36050).translate([-5500, 650]));
+
+  // default scale: 1000
+  // 647 237 current
+  
+  // 900 540 size
+  // 450 270 center
+
+
+  //mapPath = d3.geo.path().projection(d3.geo.albers());
+  //mapPath = d3.geo.path().projection(d3.geo.albers().translate([480 - 197, 250 + 33]).scale(1000));
+  mapPath = d3.geo.path().projection(d3.geo.albers().scale(180000).center([12.28, 38.226314]));
+
+  //mapPath = d3.geo.path().projection(d3.geo.albers());
+  /*
+        .projection(d3.geo.mercator()
+        .scale(20000)
+        .translate([0, 3800]));*/
+
+  mapSvg = d3.select('#map').append('svg')
+      .attr('width', 900)
+      .attr('height', 540);    
+
+  queue()
+      .defer(d3.json, 'louisville.json')
+      .await(mapIsReady);
+}
+
+function switchToState(stateName) {
+  var state = 0;
+
+  for (var i = 1; i < filters[1].choices.length; i++) {
+    if (filters[1].choices[i].title == stateName) {
+      state = i;
+      break;
+    }
+  }
+
+  if (filters[1].selected == state) {
+    filters[1].selected = 0;
+  } else {
+    filters[1].selected = state;
+  }
+
+  updateData();  
+}
+
+function mapIsReady(error, us) {
+  mapReady = true;
+
+  mapUsData = us;
+
+  mapSvg.append('g')
+    .attr('class', 'state-bound')
+
+    .selectAll('path')
+    //.data(topojson.object(mapUsData, mapUsData.objects.states).geometries)
+    .data(mapUsData.features)
+    .enter()
+    .append('path')
+    .attr('d', mapPath)
+    .attr('state', function(d) { return d.properties.name; })
+    .on('click', function() {
+      switchToState(this.getAttribute('state'));
+    })
+    .on('mouseover', function() {
+      // TODO class
+      d3.select(this)
+        .style('stroke', 'black')
+        .style('stroke-width', 3)
+    })
+    .on('mouseout', function() {
+      d3.select(this)
+      // TODO class
+        .style('stroke', '')
+        .style('stroke-width', '')
+        .style('fill', '');
+    });
+
+  updateMap();
+}
+
+//function ready(error, us, unemployment) {
+function updateMap() {
+  if (!mapReady) {
+    return;
+  }
+
+  var max = 0;
+  var map = {};
+  for (var i = 1; i < filters[1].choices.length; i++) {
+    if (max < data[1][i]) {
+      max = data[1][i];
+    }
+
+    map[filters[1].choices[i].title] = i;
+  }
+
+  var quantize = d3.scale.quantize()
+    .domain([0, max])
+    .range(d3.range(9).map(function(i) { return 'q' + i + '-9'; }));
+
+  mapSvg.selectAll('path')
+    .attr('class', function(d) { return 'state ' + quantize(data[1][map[d.properties.name]]); })
+
+  // TODO change to a class
+  if (filters[1].selected == 0) {
+    mapSvg.selectAll('path')
+      .transition().duration(DATA_TRANSITION_DELAY)
+      .attr('opacity', 1);
+  } else {
+    mapSvg.selectAll('path')
+      .transition().duration(DATA_TRANSITION_DELAY)
+      .attr('opacity', function(d) { 
+        return (filters[1].choices[filters[1].selected].title == d.properties.name) ? 1 : .25
+    });
+  }
+
+
+
+}
+
+function main() {
+  prepareMap();
+
+  prepareFilters();
+
+  createFakeData();
+  createNav();
+
+  updateData();
+}
