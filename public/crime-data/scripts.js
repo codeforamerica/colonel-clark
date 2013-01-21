7b8d4d8e179a78eea2b9b52ac3871f821075551e
Marcin Wichary <mwichary@gmail.com>
2013-01-20 18:10:47 -0800

    Added crime types + proper absolute maximum.

--- public/crime-data/scripts.js
+++ public/crime-data/scripts.js
@@ -4,14 +4,19 @@ var SORT_ORDER_NAME = 2;
 var DATA_SOURCE_2010 = 1;
 var DATA_SOURCE_2011 = 2;
 
+var DATA_TYPE_OFFENSES = 1;
+var DATA_TYPE_ARRESTS = 2;
+
 var LABEL_WIDTH = 200;
 var CHART_WIDTH = 620;
-var BAR_HEIGHT = 20;
+var BAR_HEIGHT = 22;
+var BAR_PADDING = 2;
 
-var DURATION_TIME = 1000;
+var DURATION_TIME = 500;
 
 var dataSource = DATA_SOURCE_2011;
 var sortOrder = SORT_ORDER_NAME;
+var dataType = DATA_TYPE_OFFENSES;
 
 var loadedData;
 var dataCount;
@@ -50,28 +55,33 @@ function calculateDataOrder() {
 }
 
 function prepareData() {
-  currentData = [];
+  switch (dataType) {
+    case DATA_TYPE_ARRESTS:
+      var dataTypeField = 'arrestOffensesByYear';
+      break;
+    case DATA_TYPE_OFFENSES:
+      var dataTypeField = 'offensesByYear';
+      break;
+  }
   switch (dataSource) {
     case DATA_SOURCE_2010:
-      for (var i in loadedData['offensesByYear']['2010']['part1']) {
-        currentData.push(loadedData['offensesByYear']['2010']['part1'][i]);
-      }
-      for (var i in loadedData['offensesByYear']['2010']['part2']) {
-        currentData.push(loadedData['offensesByYear']['2010']['part2'][i]);
-      }
+      var dataSourceField = '2010';
       break;
     case DATA_SOURCE_2011:
-      for (var i in loadedData['offensesByYear']['2011']['part1']) {
-        currentData.push(loadedData['offensesByYear']['2011']['part1'][i]);
-      }
-      for (var i in loadedData['offensesByYear']['2011']['part2']) {
-        currentData.push(loadedData['offensesByYear']['2011']['part2'][i]);
-      }
+      var dataSourceField = '2011';
       break;
+  }
+
+  currentData = [];
+  for (var i in loadedData[dataTypeField][dataSourceField]['part1']) {
+    currentData.push(loadedData[dataTypeField][dataSourceField]['part1'][i]);
+  }
+  for (var i in loadedData[dataTypeField][dataSourceField]['part2']) {
+    currentData.push(loadedData[dataTypeField][dataSourceField]['part2'][i]);
   }
 }
 
-function prepareChart() {
+function findDataCount() {
   dataCount = 0;
   for (var i in loadedData['offensesByYear']['2010']['part1']) {
     dataCount++;
@@ -79,8 +89,32 @@ function prepareChart() {
   for (var i in loadedData['offensesByYear']['2010']['part2']) {
     dataCount++;
   }
-  dataCount += 2;
+  dataCount += 2;    
+}
 
+function findAbsoluteMaximum() {
+  absoluteMaximum = 0;
+  for (var i in loadedData) {
+    for (var j in loadedData[i]) {
+      for (var k in loadedData[i][j]) {
+        for (var l in loadedData[i][j][k]) {
+          var val = loadedData[i][j][k][l];
+
+          if (val > absoluteMaximum) {
+            absoluteMaximum = val;
+          }
+        }
+      }
+    }
+  }
+}
+
+function analyzeData() {
+  findDataCount();
+  findAbsoluteMaximum();
+}
+
+function createChart() {
   chart = d3.select("#chart").append("svg")
       .attr("class", "chart")
       .attr("width", LABEL_WIDTH + CHART_WIDTH + 100)
@@ -98,14 +132,14 @@ function prepareChart() {
   calculateDataOrder();
 
   chartScale = d3.scale.linear()
-      .domain([0, d3.max(currentData)])
+      .domain([0, absoluteMaximum])
       .range([0, CHART_WIDTH]);
 
   chart.selectAll("rect")
       .data(currentData)
       .enter().append("rect")
       .attr("x", LABEL_WIDTH)
-      .attr("y", function(d, i) { return currentDataOrdering.indexOf(i) * BAR_HEIGHT; })
+      .attr("y", function(d, i) { return currentDataOrdering.indexOf(i) * (BAR_HEIGHT + BAR_PADDING); })
       .attr("width", chartScale)
       .attr("height", BAR_HEIGHT);
 
@@ -114,7 +148,7 @@ function prepareChart() {
       .enter().append("text")
       .attr("class", "label")
       .attr("x", 0)
-      .attr("y", function(d, i) { return currentDataOrdering.indexOf(i) * BAR_HEIGHT - 5; })
+      .attr("y", function(d, i) { return currentDataOrdering.indexOf(i) * (BAR_HEIGHT + BAR_PADDING) - 5; })
       .attr("dx", LABEL_WIDTH - 3) // padding-right
       .attr("dy", BAR_HEIGHT) // vertical-align: middle
       .text(String);             
@@ -124,7 +158,7 @@ function prepareChart() {
       .enter().append("text")
       .attr("class", "value")
       .attr("x", chartScale)
-      .attr("y", function(d, i) { return currentDataOrdering.indexOf(i) * BAR_HEIGHT - 5; })
+      .attr("y", function(d, i) { return currentDataOrdering.indexOf(i) * (BAR_HEIGHT + BAR_PADDING) - 5; })
       .attr("dx", LABEL_WIDTH + 3) // padding-right
       .attr("dy", BAR_HEIGHT) // vertical-align: middle
       .text(String);             
@@ -140,6 +174,11 @@ function changeSortOrder(newSortOrder) {
   updateChart();
 }
 
+function changeDataType(newDataType) {
+  dataType = newDataType;
+  updateChart();
+}
+
 function updateChart() {
   prepareData();
   calculateDataOrder();
@@ -149,20 +188,20 @@ function updateChart() {
       .transition()
       .duration(DURATION_TIME)
       .attr("width", chartScale)
-      .attr("y", function(d, i) { return currentDataOrdering.indexOf(i) * BAR_HEIGHT; });
+      .attr("y", function(d, i) { return currentDataOrdering.indexOf(i) * (BAR_HEIGHT + BAR_PADDING); });
 
   chart.selectAll("text.label")
       .data(currentData)
       .transition()
       .duration(DURATION_TIME)
-      .attr("y", function(d, i) { return currentDataOrdering.indexOf(i) * BAR_HEIGHT - 5; })
+      .attr("y", function(d, i) { return currentDataOrdering.indexOf(i) * (BAR_HEIGHT + BAR_PADDING) - 5; })
 
   chart.selectAll("text.value")
       .data(currentData)
       .transition()
       .duration(DURATION_TIME)
       .attr("x", chartScale)
-      .attr("y", function(d, i) { return currentDataOrdering.indexOf(i) * BAR_HEIGHT - 5; })
+      .attr("y", function(d, i) { return currentDataOrdering.indexOf(i) * (BAR_HEIGHT + BAR_PADDING) - 5; })
       .tween("text", function(d) {
         // TODO(mwichary): Must be a better way to do this.
         var i = d3.interpolateNumber(parseInt(this.textContent), d);
@@ -174,7 +213,8 @@ function updateChart() {
 
 function dataLoaded(error, data) {
   loadedData = data;
-  prepareChart();
+  analyzeData();
+  createChart();
 }
 
 function loadData() {
