75b137b3a56c2fb7051d98c764c8b154424e3b2a
ycombinator <ycombinator@gmail.com>
2013-01-23 08:13:47 -0800

    Adding hack to run process weekly (since Heroku's scheduler add-on doesn't natively support weekly granularity yet).

--- cron/etl.js
+++ cron/etl.js
@@ -3,6 +3,16 @@ var extractor = require(__dirname + '/../lib/extractor'),
     loader = require(__dirname + '/../lib/loader'),
     config = require('config');
 
+// If there is a specific day this process should be run, exit if today is not that day
+// This logic is here because Heroku cannot schedule at the weekly granularity.
+if (process.env.RUN_DAY
+    && ((new Date()).getDay() !== parseInt(process.env.RUN_DAY))) {
+
+    console.info("Skipping run since today is not the day to run this");
+    process.exit(0);
+
+}
+
 var e = extractor.createExtractor(config.extractor_source_url);
 var t = transformer.createTransformer();
 var l = loader.createLoader(process.env.DATABASE_URL || config.db_connection_string);
