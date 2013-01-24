5f9723da99619b212aff653de3d95b29aec5f3ab
ycombinator <ycombinator@gmail.com>
2013-01-24 08:31:34 -0800

    [#8] Adding REST API to delete subscription.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/api/v1/incidents', api.v1.incidents.get);
 app.get('/api/v1/incidents-summary', api.v1.incidents_summary.get);
 app.post('/api/v1/user/:email/subscriptions', api.v1.user_subscriptions.post);
 app.put('/api/v1/subscription/:id/status', api.v1.subscription_status.put);
-//app.del('/api/v1/subscription/:id', api.v1.subscriptions.del);
+app.del('/api/v1/subscription/:id', api.v1.subscription.del);
 //app.del('/api/v1/user/:id/subscriptions', api.v1.user_subscriptions.del);
 
 // Serve all other URIs as static files from the "public" directory.
---
+++ restapi/resources/v1/subscription.js
@@ -0,0 +1,64 @@
+var pg = require('pg'),
+    config = require('config');
+
+exports.del = function(req, res, next) {
+
+  pg.connect(process.env.DATABASE_URL || config.db_connection_string, function(err, client) {
+    
+    if (err) {
+      console.error(err);
+      res.send(500, { message: String(err) });
+    }
+  
+    checkSubscriptionExists(client, req, res, next);
+  
+  });
+
+}
+
+var checkSubscriptionExists = function(client, req, res, next) {
+
+    var subscriptionId = req.params.id;
+
+    var query = client.query({
+        text: 'SELECT * FROM user_subscriptions WHERE uuid = $1',
+        values: [ subscriptionId ]
+    });
+
+    query.on('error', function(err) {
+        console.error("subscription select query error = " + err);
+        res.send(500, { message: "query error = " + String(err) });
+    });
+
+    var subscription;
+    query.on('row', function(row) {
+        subscription = row;
+    });
+    
+    query.on('end', function(result) {
+        if (subscription) {
+            deleteSubscription(client, subscription, req, res, next);
+        } else {
+            res.send(404, { message: "Could not find subscription." });
+        }
+    });
+
+}
+
+var deleteSubscription = function(client, subscription, req, res, next) {
+
+    var query = client.query({
+        text: 'DELETE FROM user_subscriptions WHERE _key = $1',
+        values: [ subscription._key ]
+    });
+
+    query.on('error', function(err) {
+        console.error("subscription delete query error = " + err);
+        res.send(500, { message: "query error = " + String(err) });
+    });
+
+    query.on('end', function(result) {
+        res.send({ message: "Subscription to " + subscription.neighborhood + " deleted."});
+    });
+    
+}
