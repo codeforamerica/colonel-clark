1c38ea73c2157c9d34bcc88125dd4a5196f0625b
ycombinator <ycombinator@gmail.com>
2013-02-06 06:15:36 -0500

    Commenting out routes since we don't want this app to be exposed publicly any more.

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.use(express.compress());
 app.use(express.bodyParser());
 
 // REST API URIs
+/* Commenting out because we no longer want this app to be
+   exposed to public (Bourbon Planners decision made on 2/5/2013)
 app.get('/api/v1/crimes', api.v1.crimes.get);
 app.get('/api/v1/neighborhoods', api.v1.neighborhoods.get);
 app.get('/api/v1/incidents', api.v1.incidents.get);
@@ -15,6 +17,7 @@ app.post('/api/v1/user/:email/subscriptions', api.v1.user_subscriptions.post);
 app.put('/api/v1/subscription/:id/status', api.v1.subscription_status.put);
 app.del('/api/v1/subscription/:id', api.v1.subscription.del);
 app.del('/api/v1/user/:id/subscriptions', api.v1.user_subscriptions.del);
+*/
 
 // Click that 'Hood redirect
 app.get('/click-that-hood', function(req, res, next) {
@@ -27,7 +30,10 @@ app.get('/click-that-hood', function(req, res, next) {
 })
 
 // Serve all other URIs as static files from the "public" directory.
+/* Commenting out because we no longer want this app to be
+   exposed to public (Bourbon Planners decision made on 2/5/2013)
 app.use(express.static(__dirname + '/public'));
+*/
 
 var port = process.env.PORT || 8000;
 app.listen(port);
